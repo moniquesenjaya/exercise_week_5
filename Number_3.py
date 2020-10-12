a15b10657841be46e2d33abd1397b89820ff8b54
moniquesenjaya <70890949+moniquesenjaya@users.noreply.github.com>
2020-10-12 13:05:12 +0700

    Update Number_3.py

--- Number_3.py
+++ Number_3.py
@@ -1,3 +1,3 @@
 my_list = [('English', 88), ('Science', 90), ('Maths', 97), ('Social sciences', 82)]
-my_list.sort(key=lambda x:x[0])
-print(my_list)
+sort = lambda x: sorted(my_list)
+print(sort(my_list))
