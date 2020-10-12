846a8f524847eac26dab761a434f8dc8bc3039b2
moniquesenjaya <70890949+moniquesenjaya@users.noreply.github.com>
2020-10-12 11:38:45 +0700

    Create Number_4.py

---
+++ Number_4.py
@@ -0,0 +1,10 @@
+import datetime
+now = datetime.datetime.now()
+year = lambda x: x.year
+month = lambda x: x.month
+day = lambda x: x.day
+time = lambda x: x.time()
+print(year(now))
+print(month(now))
+print(day(now))
+print(time(now))
